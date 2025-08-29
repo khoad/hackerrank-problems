fadc18c6e63a78fb0802823cb383296f83be334b
Khoa Nguyen <kh.dang.nguyen@gmail.com>
2025-08-29 13:31:10 -0700

    Update

---
+++ easy/TruckTour.java
@@ -0,0 +1,75 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+
+
+class Result {
+
+    /*
+     * Complete the 'truckTour' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY petrolpumps as parameter.
+     */
+
+    public static int truckTour(List<List<Integer>> petrolpumps) {
+        // Write your code here
+        for (int i = 0; i < petrolpumps.size(); i++) {
+            int j = i;
+            int distanceLeft = 0;
+            while (j != i - 1 && distanceLeft >= 0) {
+                distanceLeft +=  petrolpumps.get(j).get(0);
+                distanceLeft -=  petrolpumps.get(j).get(1);
+                j++;
+                if (j == petrolpumps.size()) {
+                    j = 0;
+                }
+            }
+            if (distanceLeft >= 0) {
+                return i;
+            }
+        }
+        return 0;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> petrolpumps = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                petrolpumps.add(
+                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                        .map(Integer::parseInt)
+                        .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = Result.truckTour(petrolpumps);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}

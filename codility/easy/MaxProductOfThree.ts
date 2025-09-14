be3288365be802328d62180386ca35c7d34e996e
Khoa Nguyen <kh.dang.nguyen@gmail.com>
2025-09-14 16:22:41 -0700

    Update

--- codility/easy/MaxProductOfThree.ts
+++ codility/easy/MaxProductOfThree.ts
@@ -5,7 +5,7 @@ import assert from "assert"
 
 function maxProductOfThree(A: number[]): number {
     // Implement your solution here
-    let max = 0
+    let max = Number.MIN_SAFE_INTEGER
     let l = A.length
     A.sort((a, b) => a - b)
     max = Math.max(max, A[l - 1] * A[l - 2] * A[l - 3])
@@ -15,3 +15,4 @@ function maxProductOfThree(A: number[]): number {
 
 assert.equal(maxProductOfThree([-3, 1, 2, -2, 5, 6]), 60)
 assert.equal(maxProductOfThree([-5, 5, -5, 4]), 125)
+assert.equal(maxProductOfThree([-10, -2, -4]), -80)

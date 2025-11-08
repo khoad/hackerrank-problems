6ee4f53c148efca7115ba583e738dbdac7c0fbc0
Khoa Nguyen <kh.dang.nguyen@gmail.com>
2025-11-07 17:07:53 -0800

    Update

---
+++ leetcode/easy/35-SearchInsertPosition.ts
@@ -0,0 +1,19 @@
+function searchInsert(nums: number[], target: number): number {
+    let left = 0
+    let right = nums.length - 1
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2)
+
+        if (target == nums[mid]) {
+            return mid
+        }
+
+        if (target <= nums[mid]) {
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return left
+};
